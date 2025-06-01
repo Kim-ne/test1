5589368abe50324e5a73e36b625661418eb01996
TuanBHA <anhtuan1411994@gmail.com>
2025-06-01 19:34:22 +0700

    remove duplicate output lines in test.php

--- module1/test/test.php
+++ module1/test/test.php
@@ -1,13 +1,2 @@
 <?php
 echo "This is a test file in the module1/test directory.\n";
-echo "This is a test file in the module1/test directory.\n";
-echo "This is a test file in the module1/test directory.\n";
-echo "This is a test file in the module1/test directory.\n";
-echo "This is a test file in the module1/test directory.\n";
-echo "This is a test file in the module1/test directory.\n";
-echo "This is a test file in the module1/test directory.\n";
-echo "This is a test file in the module1/test directory.\n";
-echo "This is a test file in the module1/test directory.\n";
-echo "This is a test file in the module1/test directory.\n";
-echo "This is a test file in the module1/test directory.\n";
-echo "This is a test file in the module1/test directory.\n";
